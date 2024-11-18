610701136b82db650a9136852950ae65fa59dd4c
ononama <ononama@hotmail.co.jp>
2024-11-19 03:09:39 +0900

    wp修正2

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',  // 静的サイトのエクスポートを有効にする
-
     // webpack 設定
     webpack: function (config) {
       config.module.rules.push({
